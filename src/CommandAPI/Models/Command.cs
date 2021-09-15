8b894609076e567bc48bb24ab33a7fd0b7a4dc66
Hector <heortiz@invi.gob.do>
2021-09-15 15:27:11 -0400

    Add models

--- src/CommandAPI/Models/Command.cs
+++ src/CommandAPI/Models/Command.cs
@@ -1,12 +1,21 @@
-CommandAPI.Models
+using System.ComponentModel.DataAnnotations;
 
 namespace CommandAPI.Models
 {
- public class Command
- {
- public int Id {get; set;}
- public string HowTo {get; set;}
- public string Platform {get; set;}
- public string CommandLine {get; set;}
- }
+    public class Command
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(250)]
+        public string HowTo { get; set; }
+
+        [Required]
+        public string Platform { get; set; }
+
+       
+        public string CommandLine { get; set; }
+    }
 }
