eab6ad9f98cd91ae7bcce01029cfd47bf58a53c5
Hector <heortiz@invi.gob.do>
2021-09-15 15:15:37 -0400

    Added Command Model to API Projec

---
+++ src/CommandAPI/Models/Command.cs
@@ -0,0 +1,12 @@
+CommandAPI.Models
+
+namespace CommandAPI.Models
+{
+ public class Command
+ {
+ public int Id {get; set;}
+ public string HowTo {get; set;}
+ public string Platform {get; set;}
+ public string CommandLine {get; set;}
+ }
+}
