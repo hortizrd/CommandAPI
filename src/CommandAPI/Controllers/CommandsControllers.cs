fb68c07a6caf601815ad862e5902e773b2727061
Hector <heortiz@invi.gob.do>
2021-09-20 15:45:11 -0400

    Update Dto

--- src/CommandAPI/Controllers/CommandsControllers.cs
+++ src/CommandAPI/Controllers/CommandsControllers.cs
@@ -5,6 +5,10 @@ using CommandAPI.Models;
 using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
 using System.Linq;
+using AutoMapper;
+using CommandAPI.Dtos;
+using System;
+
 namespace CommandAPI.Controllers
 {
     [Route("api/[controller]")]
@@ -12,33 +16,39 @@ namespace CommandAPI.Controllers
     public class CommandsController : ControllerBase
     {
         private readonly ICommandAPIRepo _repository;
-        public CommandsController(ICommandAPIRepo repository) {
+        private readonly IMapper _mapper;
+        public CommandsController(ICommandAPIRepo repository, IMapper mapper) {
             _repository = repository;
+            _mapper = mapper;
 
         }
         [HttpGet]
-        public ActionResult<IEnumerable<Command>> GetAllCommands()
+        public ActionResult<IEnumerable<CommandReadDto>> GetAllCommands()
         {
             var commandItems = _repository.GetAllCommands();
 
-            return Ok(commandItems);
+            return Ok(_mapper.Map<IEnumerable<CommandReadDto>>(commandItems));
         }
-        //[HttpGet]
-        //public async Task<IActionResult> GetAll()
-        //{
-        //    var students = await _repository.Command.ToListAsync();
-        //    if (students == null) return NotFound();
-        //    return Ok(students);
-        //}
+      
 
 
-        //[HttpGet("{id}")]
-        //public async Task<IActionResult> GetById(int id)
-        //{
-        //    var student = await _repository.Command.Where(a => a.Id == id).FirstOrDefaultAsync();
-        //    if (student == null) return NotFound();
-        //    return Ok(student);
-        //}
+        [HttpGet("{id}")]
+        public ActionResult<CommandReadDto> GetById(int id)
+        {
+            var commandItem = _repository.GetCommandById(id);
+            if (commandItem == null) return NotFound();
+            return Ok(_mapper.Map<CommandReadDto>(commandItem));
+        }
 
+       [HttpPost]
+       public ActionResult<CommandReadDto>CreateCommand(CommandCreateDto commandCreateDto)
+        {
+            var commandModel = _mapper.Map<Command>(commandCreateDto);
+            _repository.CreateCommand(commandModel);
+            _repository.SaveChanges();
+
+            var commandReadDto = _mapper.Map<CommandReadDto>(commandModel);
+            return CreatedAtRoute(nameof(GetById), new { Id = commandReadDto.Id }, commandReadDto);
+        }
     }
 }
